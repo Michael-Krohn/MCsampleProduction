0b2d690506d30ca2d7e1a36e62c5444f4c67a9b7
Si Hyun Jeon <shjeon@cern.ch>
2020-12-23 00:52:32 +0900

    update SIM

--- packages/FullSimulation/SIM/config_SIM.py
+++ packages/FullSimulation/SIM/config_SIM.py
@@ -1,6 +1,5 @@
 import os
 import sys
-import random
 import argparse
 
 def exit_argumenterr():
@@ -11,13 +10,14 @@ def exit_argumenterr():
         print "[EXIT] e.g.) TTbarTypeIHeavyN-Mu_4L_LO_MN60 /TTbarTypeIHeavyN-Mu_4L_LO_MN60/jihunk-DRPremix_step1__CMSSW_10_2_5-96e2d90999375d8c542ea905b43803e1/USER"
 	sys.exit()
 
-def print_sampleinfo(datasetname,outputdataset):
+def print_sampleinfo(datasetname,inputdataset,inputname):
+	print "[INFO] Reading list of samples to be submitted : "+inputname
         print "[INFO] Generating configuration files for "+datasetname
-        print "[INFO]      Using "+outputdataset
-        check_arguement(datasetname)
+        print "[INFO]      Using "+inputdataset
+        check_argument(datasetname)
 
 def check_argument(datasetname):
-	if not os.path.isdir(datasetname):
+	if os.path.isdir(datasetname):
 	  print "[EXIT] Directory "+datasetname+" already exists"
 	  sys.exit()
 
@@ -25,6 +25,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("input_f")
 parser.add_argument("--dev", action = "store_true")
 input_f = parser.parse_args().input_f
+inputname = input_f.split(".")[0]
 dev = parser.parse_args().dev
 
 cwd = os.getcwd()
@@ -45,48 +46,60 @@ for cmsdriver_campaign_l in cmsdriver_campaign_ls:
 
 cmsdriver_campaign_f.close()
 
-os.system("mkdir -p Configuration/GenProduction/python/")
 submit_list = []
+
+run_cmsdriver_sh = open("run_cmsdriver_"+inputname+".sh","w")
+run_cmsdriver_sh.write("#!/bin/bash\n")
+run_cmsdriver_sh.write("source /cvmfs/cms.cern.ch/cmsset_default.sh\n")
+run_cmsdriver_sh.write("cmsenv\n")
+run_cmsdriver_sh.write("scram b -j 4\n")
+
+submit_crab_sh = open("submit_crab_"+inputname+".sh","w")
 
 for list_l in list_ls:
   list_l = list_l.strip().replace(" ",",").replace("\t",",").split(",")
   if len(list_l) != 2:
     exit_argumenterr()
   datasetname = list_l[0]
-  outputdataset = list_l[1]
+  inputdataset = list_l[1]
 
-  print_sampleinfo(datasetname,outputdataset)
+  print_sampleinfo(datasetname,inputdataset,inputname)
 
   submit_list.append(datasetname)
-  os.system("mkdir -p "+datasetname)
+  crabwd = inputname+"/"+datasetname+"/"
+  os.system("mkdir -p "+crabwd)
 
-  os.system("cp skeleton/submit_crab.py "+datasetname+"/")
-  os.system("sed -i 's|###REQUESTNAME###|"+datasetname+"|g' "+datasetname+"/submit_crab.py")
-  os.system("sed -i 's|###OUTPUTPRIMARYDATASET###|"+datasetname+"|g' "+datasetname+"/submit_crab.py")
-  os.system("sed -i 's|###OUTPUTDATASETTAG###|"+campaign+"_"+step+"|g' "+datasetname+"/submit_crab.py")
-  os.system("sed -i 's|###UNITSPERJOB###|"+str(int(nevents)/int(nsplitjobs))+"|g' "+datasetname+"/submit_crab.py")
-  os.system("sed -i 's|###NJOBS###|NJOBS="+nsplitjobs+"|g' "+datasetname+"/submit_crab.py")
+  os.system("cp skeleton/submit_crab.py "+crabwd+"/submit_crab.py")
+  os.system("sed -i 's|###REQUESTNAME###|"+datasetname+"|g' "+crabwd+"/submit_crab.py")
+  os.system("sed -i 's|###INPUTDATASET###|"+inputdataset+"|g' "+crabwd+"/submit_crab.py")
+  os.system("sed -i 's|###OUTPUTDATASETTAG###|"+campaign+"_"+step+"|g' "+crabwd+"/submit_crab.py")
+#  os.system("sed -i 's|###UNITSPERJOB###|"+str(int(nevents)/int(nsplitjobs))+"|g' "+datasetname+"/submit_crab.py")
 
-  os.system("mkdir -p "+datasetname)
-  cmsdriver_sh = open(datasetname+"/run_cmsdriver.sh","w")
-  cmsdriver_l = "cmsDriver.py step1 --no_exec --mc --python_filename run_crab.py --fileout "+step+".root --eventcontent RAWSIM --datatier GEN-SIM --runUnscheduled --step SIM --geometry DB:extended -n 6284 "+add_cmsdriver 
+  cmsdriver_sh = open(crabwd+"/run_cmsdriver.sh","w")
+  cmsdriver_l = "cmsDriver.py step1 --no_exec --mc --python_filename run_crab.py --fileout "+step+".root --eventcontent RAWSIM --datatier GEN-SIM --runUnscheduled --step SIM --geometry DB:Extended -n 6284 "+add_cmsdriver 
   cmsdriver_sh.write("#!/bin/bash\n")
   cmsdriver_sh.write(cmsdriver_l+"\n")
   cmsdriver_sh.close()
 
-run_cmsdriver_sh = open("run_cmsdriver_"+input_f.split(".")[0]+".sh","w")
-run_cmsdriver_sh.write("#!/bin/bash\n")
-run_cmsdriver_sh.write("source /cvmfs/cms.cern.ch/cmsset_default.sh\n")
-run_cmsdriver_sh.write("cmsenv\n")
-for i_submit in range(len(submit_list)):
-  run_cmsdriver_sh.write("cd "+cwd+"/"+datasetname+"/\n")
-  run_cmsdriver_sh.write("crab submit -c submit_crab.py\n")
-run_cmsdriver_sh.write("cd "+cwd+"/"+datasetname+"/\n")
+  run_cmsdriver_sh.write("cd "+cwd+"/"+crabwd+"/\n")
+  run_cmsdriver_sh.write("chmod a+x ./run_cmsdriver.sh\n")
+  run_cmsdriver_sh.write("source ./run_cmsdriver.sh\n")
+
+  submit_crab_sh.write("cd "+cwd+"/"+crabwd+"/\n")
+  submit_crab_sh.write("crab submit -c submit_crab.py\n")
+
+run_cmsdriver_sh.write("cd "+cwd+"/\n")
 run_cmsdriver_sh.close()
 
-os.system("source run_cmsdriver_"+input_f.split(".")[0]+".sh")
+submit_crab_sh.write("cd "+cwd+"/\n")
+submit_crab_sh.close()
+
+os.system("source ./run_cmsdriver_"+inputname+".sh")
+os.system("rm ./run_cmsdriver_"+inputname+".sh")
+
 print "[INFO] cmsDriver build for datasets below have completed"
 for i_submit in range(len(submit_list)):
-  print "[INFO]      "+submit_list[i_submit]
+  print "[INFO] >>      "+submit_list[i_submit]
 print "[INFO] Execute the command to submit the jobs to CRAB"
-print "[INFO]      source submit_crab_"+input_f.split(".")[0]+".sh"
+print "[INFO] >>     voms-proxy-init"
+print "[INFO] >>     source submit_crab_"+inputname+".sh"
--- packages/FullSimulation/SIM/skeleton/submit_crab.py
+++ packages/FullSimulation/SIM/skeleton/submit_crab.py
@@ -7,18 +7,20 @@ config.General.workArea = "crab_projects"
 config.General.transferLogs = True
 config.General.transferOutputs = True
 
-config.JobType.pluginName = "PrivateMC"
+config.JobType.pluginName = "Analysis"
 config.JobType.psetName = "run_crab.py"
 #config.JobType.maxMemoryMB = 4000
 #config.JobType.numCores = 8
 
-config.Data.outputPrimaryDataset = "###OUTPUTPRIMARYDATASET###"
-config.Data.outLFNDirBase = "/store/user/%s/" % (getUsernameFromCRIC())#FIXME
+config.Data.inputDataset = "###INPUTDATASET###" 
+config.Data.outLFNDirBase = "/store/user/%s/" % (getUsernameFromCRIC())
 config.Data.outputDatasetTag = "###OUTPUTDATASETTAG###"
-config.Data.splitting = 'EventBased'
-config.Data.unitsPerJob = ###UNITSPERJOB###
-###NJOBS###
-config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
+config.Data.inputDBS = "phys03"
+config.Data.splitting = "FileBased"
+config.Data.unitsPerJob = 1
 config.Data.publication = True
+config.Data.ignoreLocality = True
 
-config.Site.storageSite = 'T2_CH_CERN'
+config.Site.storageSite = "T3_KR_KNU"
+config.Site.whitelist = ["T2_*","T3_*"]
+
