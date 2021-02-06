3ebcbbf5e657e4c2713ae5677309e2bca4f24215
Si Hyun Jeon <shjeon@cern.ch>
2021-02-06 21:32:40 +0900

    update setup

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # exo-FastsimStudies
 
-gitlab repository for EXO-MC&I fast simulation studies
+Steps : (wmLHE)GEN >> SIM >> DIGIPremix >> HLT >> RECO >> MiniAOD >> NanoAODv2
+For more details check out :
+    EXO-MC&I : https://exo-mc-and-i.gitbook.io/exo-mc-and-interpretation/
+    PdmV : https://cms-pdmv.gitbook.io/project/
--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
+#############################################################
+# Fast and Full simulation sample production
+# Author : Sihyun Jeon (shjeon@cern.ch)
+#############################################################
+
 import os
 import sys
 
@@ -8,31 +13,26 @@ cmssws = ["slc7_amd64_gcc700", "slc7_amd64_gcc700", "slc7_amd64_gcc700", "slc7_a
 cmssws_HLT = ["slc7_amd64_gcc530", "slc7_amd64_gcc530", "slc7_amd64_gcc630", "slc7_amd64_gcc700"]
 methods = ["Full"]#, "Fast"]
 
-print "#############################################################################"
-print "#############################################################################"
-print ""
-print "       Setting up Fast and Full simulation sample production workflows       "
-print "       author : Sihyun Jeon (shjeon@cern.ch)                                 "
-print ""
-print "#############################################################################"
-print "#############################################################################"
+print "Setting up Fast and Full simulation sample production workflows."
+config_storagesite = raw_input("What is your T2/T3 storage site [T2_CH_CERN,T3_KR_KNU,T2_US_FNAL,..]? ")
 
 setup_sh = open("setup.sh", "w")
 setup_sh.write("source /cvmfs/cms.cern.ch/cmsset_default.sh\n")
 
+print "Fetching CMSSW releases for Ultra Legacy sample production."
 for i_method in range(0,len(methods)):
   method = methods[i_method]
 
   for i_campaign in range(0,len(campaigns)):
     campaign = campaigns[i_campaign]
-    print "Fetching CMSSW release versions for Ultra Legacy "+method+" "+campaign+"..."
+#    print "Fetching CMSSW releases for Ultra Legacy "+method+" "+campaign+"..."
     setup_f = open(cwd+"/packages/setups/"+method+"/setup_"+campaign+".dat")
     setup_ls = setup_f.readlines()
     setup_f.close()
 
     for i_setup in range(0,len(setup_ls)):
       setup_l =  setup_ls[i_setup].strip()
-      print "\t"+setup_l
+#      print "\t"+setup_l
       step = setup_l.split("\t")[0]
       cmssw = setup_l.split("\t")[1]
       setup_sh.write("mkdir -p "+cwd+"/"+method+"Simulation/"+campaign+"/\n")
@@ -46,7 +46,20 @@ for i_method in range(0,len(methods)):
       setup_sh.write("cp -r "+cwd+"/packages/"+method+"Simulation/"+step+"/* .\n")
 
 setup_sh.write("cd "+cwd+"\n")
+setup_sh.write("sed -i 's|###CONFIG_STORAGESITE###|{}|g' ./*Simulation/RunIISummer20UL*/*/src/skeleton/submit_crab.py".format(config_storagesite))
 setup_sh.close()	
 os.system("chmod 755 setup.sh")
 os.system("source ./setup.sh")
+print "Fetching complete."
+print "Steps : (wmLHE)GEN >> SIM >> DIGIPremix >> HLT >> RECO >> MiniAOD >> NanoAODv2"
+print "For more details check out :"
+print "    EXO-MC&I : https://exo-mc-and-i.gitbook.io/exo-mc-and-interpretation/"
+print "    PdmV : https://cms-pdmv.gitbook.io/project/"
+print "Make sure you check your writing permissions by executing the command :"
+print "    source /cvmfs/cms.cern.ch/crab3/crab.sh"
+print "    source /cvmfs/cms.cern.ch/cmsset_default.sh"
+print "    cmsrel CMSSW_X_Y_Z"
+print "    cd CMSSW_X_Y_Z/src/"
+print "    cmsenv"
+print "    crab checkwrite --site={}".format(config_storagesite)
 os.system("rm setup.sh")
